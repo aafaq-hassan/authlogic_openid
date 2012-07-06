e8cbc98ab978df31ebd1a7f8d2dfb64d4f0d16ed
aafaq-hassan <aafaqhassan@gmail.com>
2012-07-07 03:13:09 +0600

    save method is corrected to save record.

--- lib/authlogic_openid/acts_as_authentic.rb
+++ lib/authlogic_openid/acts_as_authentic.rb
@@ -71,8 +71,6 @@ module AuthlogicOpenid
       # if their OpenID provider supports it.
       def save(perform_validation = true, &block)
         return false if perform_validation && block_given? && authenticate_with_openid? && !authenticate_with_openid
-        
-        return false if new_record? && !openid_complete?
         result = super
         yield(result) if block_given?
         result
